a731a37e076e75b7ae7d8123f6e9755ada390e8f
henhentabs <hearttytabz@gmail.com>
2024-03-12 23:09:13 +0800

    Improving the UI

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAlert, IonButton, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,18 +8,65 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+        
+    <IonCard>
+      <img alt="Silhouette of mountains" src="../src/assets/img/avatar.jpg" />
+      <img alt="Silhouette of mountains" id="profile_pic" src="../src/assets/img/HEN_CIRCLE_PINK_PNG.png" />  
+        <IonCardHeader>
+          <IonCardTitle>Hencez Heart M. Taborno</IonCardTitle>
+           <IonCardSubtitle>BSIT Student</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent><i>❤️ Be All You Can!</i> 😊 🙏 &#x1F446;</IonCardContent>
+
+      <IonButton id="present-alert" expand="block" color="primary">Quick Facts</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="Explore Quick Facts?"
+        subHeader="It's worth your time."
+        message="Discovering quick facts will provide comprehensive insights about me."
+        buttons={['Proceed']}
+      ></IonAlert>
+
+<IonButton id="open-action-sheet" expand="block" color="primary">Open Action Sheet</IonButton>
+      <IonActionSheet
+        trigger="open-action-sheet"
+        header="Actions"
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Share',
+            data: {
+              action: 'share',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
+    </IonCard>
+        </IonContent>
+      </IonPage>
   );
 };
 
