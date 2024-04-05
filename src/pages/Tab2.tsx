39be61bfa711f0e05c99979daa60b43056376f25
henhentabs <hearttytabz@gmail.com>
2024-04-05 17:33:26 +0800

    added content to dev-clickcounter

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 
-const Calculator: React.FC = () => {
+const ClickCounter: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Calculator</IonTitle>
+          <IonTitle>Click Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        {/* Your content here */}
+      <IonContent fullscreen className="ion-padding">
+        <div className="ion-text-center">
+          <h1>Count: {count}</h1>
+          <IonButton onClick={incrementCount}>Increment</IonButton>
+          <IonButton onClick={decrementCount}>Decrement</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Calculator;
+export default ClickCounter;
