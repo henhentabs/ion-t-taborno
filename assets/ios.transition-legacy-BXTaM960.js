3bd66d6b4c7a21b4acaf08a0bbbf915d3ee19ea8
henhentabs <hearttytabz@gmail.com>
2024-05-07 08:54:25 +0800

    Updates

--- assets/ios.transition-legacy-BXTaM960.js
+++ assets/ios.transition-legacy-BXTaM960.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D9u13AXC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-D3RIjI7A.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
