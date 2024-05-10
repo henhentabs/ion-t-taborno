e1d4c3d34f3f5a18961f62caf4952157b4506c6b
henhentabs <hearttytabz@gmail.com>
2024-05-10 20:22:04 +0800

    fixing profile images issues

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -124,7 +124,7 @@ const Profile: React.FC = () => {
             id="modal_cover_pic"
             className="custom-modal"
           >
-            <img alt="Silhouette of mountains" id="coverpic" src="../src/assets/img/COVER_PIC_WHOLE_JPG.jpg" />
+            <img alt="Silhouette of mountains" id="coverpic" src="https://raw.githubusercontent.com/henhentabs/ion-t-taborno/main/src/assets/img/COVER_PIC_WHOLE_JPG.jpg" />
             <IonButton color="light" id="modal_coverpic_close_button" onClick={() => setShowModal_coverpic(false)}>X</IonButton>
           </IonModal>
           
@@ -135,7 +135,7 @@ const Profile: React.FC = () => {
             className="custom-modal"
           >
             {/* <img alt="Silhouette of mountains" src="../src/assets/img/HENHEN_JPG.jpg" /> */}
-            <img alt="Silhouette of mountains" id="profpic" src="../src/assets/img/HEN_CIRCLE_PINK_PNG.png" />
+            <img alt="Silhouette of mountains" id="profpic" src="https://raw.githubusercontent.com/henhentabs/ion-t-taborno/main/src/assets/img/HEN_CIRCLE_PINK_PNG.png" />
             <IonButton color="light" id="modal_profpic_close_button" onClick={() => setShowModal_profpic(false)}>X</IonButton>
           </IonModal>
 
