52c465211b9b02c6242354ef1cb9106f9f488c40
henhentabs <hearttytabz@gmail.com>
2024-05-10 20:08:20 +0800

    Updates

--- assets/ios.transition-legacy-DZ-nK16Q.js
+++ assets/ios.transition-legacy-DZ-nK16Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgxUoebQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-vfkzwG-d.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
